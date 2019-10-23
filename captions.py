275db3fc0771a162b24491976137b0343bbc01a5
andrew Follmann <follmann.andrew@gmail.com>
2019-10-23 09:24:00 -0400

    turned it into class, add channel scraper

--- captions.py
+++ captions.py
@@ -1,108 +1,105 @@
 # modify these values
-filename = 'videolist_zembla_273_2018_05_25-09_17_02.tab'			# filname with video ids
-colname = 'videoId'													# column storing video ids
-delimiter = '\t'													# delimiter, e.g. ',' for CSV or '\t' for TAB
-waittime = 10														# seconds browser waits before giving up
-sleeptime = [5,15]													# random seconds range before loading next video id
-headless = True														# select True if you want the browser window to be invisible (but not inaudible)
-
-#do not modify below
-from time import sleep
-import csv
-import random
-import os.path
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+
+import os
+import re
+import random
+import pandas as pd
+from time import sleep
+filename = 'meta_input.csv'			# filname with video ids
+colname = 'video_id'					# column storing video ids
+waittime = 10						# seconds browser waits before giving up
+# random seconds range before loading next video id
+sleeptime = [5, 15]
+
+headless = True
 
 
-def gettranscript(videoid):
+OUTDIR = './transcripts/'
+filepattern = 'tanscript_{}.txt'
+OPTIONS = webdriver.ChromeOptions()
+#OPTIONS.headless=True
 
-	# check if transcript file already exists	
-	writefilename = 'subtitles/transcript_' + videoid + '.txt'
-	if os.path.isfile(writefilename):
-		msg = 'transcript file already exists'
-		return msg
 
-	sleep(random.uniform(sleeptime[0],sleeptime[1]))
+def random_wait(smin=5, smax=15):
+    sleep(random.uniform(smin, smax))
 
-	options = Options()
-	options.add_argument("--headless")
 
-	# Create a new instance of the Firefox driver
-	if headless:
-		driver = webdriver.Firefox(firefox_options=options)
-	else:
-		driver = webdriver.Firefox()
 
-	# navigate to video
-	driver.get("https://www.youtube.com/watch?v="+videoid)
+YOUTUBE_BASE = "https://www.youtube.com/watch?v="
+OUTER_FRAME_XPATH = '//body[@dir="ltr"]'
 
-	try:
-	    element = WebDriverWait(driver, waittime).until(EC.presence_of_element_located((By.CSS_SELECTOR, "yt-icon-button.dropdown-trigger > button:nth-child(1)")))
-	except:
-		msg = 'could not find options button'
-		driver.quit()
-		return msg
+class TranscriptDriver:
+    MENU_TRIGGER = '//div[@id="container"]//yt-icon-button[@class="dropdown-trigger style-scope ytd-menu-renderer"]'
+    TRANSCRIPT_BUTTON = '(//ytd-menu-popup-renderer//*/yt-formatted-string)[2]'
+    FULL_TRANSCRIPT_XPATH = '//ytd-transcript-body-renderer'
+    CUE_XPATH = '//ytd-transcript-body-renderer//div[@class="cues style-scope ytd-transcript-body-renderer"]'
+    CUE_TIME_XPATH = '//ytd-transcript-body-renderer//div[@class="cue-group-start-offset style-scope ytd-transcript-body-renderer"]'
 
-	try:
-		element.click()
-	except:
-		msg = 'could not click'
-		driver.quit()
-		return msg
+    def __init__(self, video_id):
+        self.video_id = video_id
+        self.csv_path = 'transcripts/timed/{}.csv'.format(self.video_id)
+        self.txt_path = 'transcripts/untimed/{}.txt'.format(self.video_id)
+        print("Begin", video_id)
+        self.driver = None
+        self.default_waittime = 3
+        self.n_tries = 5
 
-	try:
-	    element = WebDriverWait(driver, waittime).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#items > ytd-menu-service-item-renderer:nth-child(2) > yt-formatted-string"))) #items > ytd-menu-service-item-renderer:nth-child(2) > yt-formatted-string
-	except:
-		msg = 'could not find transcript in options menu'
-		driver.quit()
-		return msg
+    def wait_for_element(self, until, message='', waittime=None):
+        if waittime is None:
+            waittime = self.default_waittime
+        return WebDriverWait(self.driver, waittime).until(until)
+        # except TimeoutException:
+            #self.driver.find_element_by_xpath(OUTER_FRAME_XPATH).send_keys(Keys.ESCAPE)
+            # return WebDriverWait(self.driver, waittime).until(until)
 
-	try:
-		element.click()
-	except:
-		msg = 'could not click'
-		driver.quit()
-		return msg
+    def get_transcript(self):
+        self.driver = webdriver.Chrome(chrome_options=OPTIONS)
+        for i in range(self.n_tries):
+            try:
+                self._get_transcript()
+                return 'Success'
+            except (ElementClickInterceptedException, TimeoutException):
+                print('Failed %s times' % (i + 1))
+                continue
+        return 'Fail'
 
-	try:
-	    element = WebDriverWait(driver, waittime).until(EC.presence_of_element_located((By.CSS_SELECTOR, "ytd-transcript-body-renderer.style-scope")))
-	except:
-		msg = 'could not find transcript text'
-		driver.quit()
-		return msg
+    def _get_transcript(self):
+        self.driver.get(YOUTUBE_BASE + self.video_id)
+        random_wait(2,5)
 
-	#print(element.text)
+        menu_button = self.wait_for_element(EC.element_to_be_clickable((By.XPATH,self.MENU_TRIGGER)),'Menu Button')
+        menu_button.click()
 
-	file = open(writefilename,"w")
-	file.write(element.text)
-	file.close() 
+        transcript_button = self.wait_for_element(EC.element_to_be_clickable((By.XPATH,self.TRANSCRIPT_BUTTON)),'Transcript Button')
+        transcript_button.click()
 
-	driver.quit()
+        transcript_element = self.wait_for_element(EC.presence_of_element_located((By.XPATH, self.FULL_TRANSCRIPT_XPATH)),
+                                                   'Transcript', 10)
+        transcript_raw = transcript_element.text
+        text = re.split('\d*:\d{2}\n', transcript_raw)[1:]
+        times = re.findall('\d*:\d{2}\n', transcript_raw)
+        df = pd.DataFrame(zip(times,text),columns=['time','text']).replace('\n', '')
+        df['text'] = df['text'].str.replace('\n', '')
+        df['time'] = df['time'].str.replace('\n', '')
+        df.to_csv(self.csv_path,encoding='utf_8')
+        with open(self.txt_path, 'w',encoding='utf_8',errors='ignore') as txt:
+            txt.write(' '.join(df['text']))
 
-	return 'ok'
 
-# log function
-def logit(id,msg):
-	logwriter.writerow({'id':id,'msg':msg})
-	
 
-# prepare log file
-logwrite = open('captions.log','w',newline='\n')
-logwriter = csv.DictWriter(logwrite, fieldnames=['id','msg'])
-logwriter.writeheader()
+def get_transcript(video_id):
+    td = TranscriptDriver(video_id)
+    if os.path.exists(td.csv_path) and os.path.exists(td.txt_path):
+        return
+    td.get_transcript()
+    td.driver.quit()
 
-# read CSV file
-csvread = open(filename, newline='\n')
-csvreader = csv.DictReader(csvread, delimiter=delimiter, quoting=csv.QUOTE_NONE)
-rowcount = len(open(filename).readlines())
 
-for row in csvreader:
-	msg = gettranscript(row[colname])
-	logit(row[colname],msg)
-	rowcount -= 1
-	print(str(rowcount) + " :  " + row[colname] + " : " + msg)
+df = pd.read_csv('channels/video links UC2PA-AKmVpU6NKCGtZq_rKQ.csv')
+df[colname].apply(lambda x: get_transcript(x))
---
+++ channels.py
@@ -0,0 +1,86 @@
+# modify these values
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+
+import os.path
+import random
+import csv
+import pandas as pd
+from time import sleep
+filename = 'channels.csv'			# filname with video ids
+colname = 'channel_id'					# column storing video ids
+waittime = 10						# seconds browser waits before giving up
+# random seconds range before loading next video id
+sleeptime = [5, 15]
+
+headless = True
+
+
+OUTDIR = './transcripts/'
+filepattern = 'tanscript_{}.txt'
+OPTIONS = webdriver.ChromeOptions()
+OPTIONS.headless=True
+
+YOUTUBE_BASE = "https://www.youtube.com/channel/{}/videos"
+OUTER_FRAME_XPATH = '//body[@dir="ltr"]'
+
+def random_wait(smin=5, smax=15):
+    sleep(random.uniform(smin, smax))
+
+class ChannelDriver:
+    THUMBNAIL_LINK = '//a[@class="yt-simple-endpoint inline-block style-scope ytd-thumbnail"]'
+
+    def __init__(self, channel_id):
+        print("Begin",channel_id)
+        self.channel_id = channel_id
+        self.write_file = 'channels/video links {}.csv'.format(self.channel_id)
+        if os.path.exists(self.write_file):
+            return
+        self.driver = None
+        self.default_waittime = 3
+
+    def wait_for_element(self, until, message=''):
+        try:
+            return WebDriverWait(self.driver, self.default_waittime).until(until)
+        except TimeoutException:
+            self.driver.find_element_by_xpath(OUTER_FRAME_XPATH).send_keys(Keys.ESCAPE)
+            return WebDriverWait(self.driver, self.default_waittime).until(until)
+
+    def go_to_channel(self):
+        self.driver.get(YOUTUBE_BASE.format(self.channel_id))
+        random_wait(5, 6)
+
+    def main(self):
+        self.driver = webdriver.Chrome(chrome_options=OPTIONS)
+        self.go_to_channel()
+        self.get_videos()
+
+    def get_videos(self, n=10):
+        video_ids = []
+        main_page = self.wait_for_element(EC.element_to_be_clickable((By.XPATH, OUTER_FRAME_XPATH)))
+        for i in range(n):
+            main_page.send_keys(Keys.PAGE_DOWN, Keys.PAGE_DOWN)
+            sleep(.5)
+        link_elements = self.wait_for_element(EC.presence_of_all_elements_located((By.XPATH, self.THUMBNAIL_LINK)))
+        links = [link.get_attribute('href') for link in link_elements]
+        video_ids += [link.split('=')[-1] for link in links if link is not None]
+        df = pd.DataFrame(data=video_ids, columns=['video_id'])
+        df['channel_id'] = self.channel_id
+        df.to_csv(self.write_file)
+
+
+
+def get_videos(channel_id):
+    td = ChannelDriver(channel_id)
+    if os.path.exists(td.write_file):
+        return
+    return td.main()
+
+
+df = pd.read_csv(filename)
+df[colname].apply(lambda x: get_videos(x))
